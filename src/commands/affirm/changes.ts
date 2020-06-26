39e938a40206deb0581c9af1f08b4c62684a63cf
Drew Snyder <drewtsnyder@gmail.com>
2020-06-26 13:24:04 -0600

    changes command working

--- src/commands/affirm/changes.ts
+++ src/commands/affirm/changes.ts
@@ -41,10 +41,11 @@ export default class Changes extends SfdxCommand {
     ];
 
     public static args = [{ branch: 'file' }];
-
+    // TODO: change flag names and descriptions from only to something else as it will print more than one if more than one flag is provided.
     protected static flagsConfig = {
         // flag with a value (-n, --name=VALUE)
         branch: flags.string({ char: 'b', description: messages.getMessage('branchFlagDescription') }),
+        inputdir: flags.string({ char: 'n', description: messages.getMessage('inputdirFlagDescription') }),
         onlydestructive: flags.boolean({ char: 'd', description: messages.getMessage('onlydestructiveFlagDescription') }),
         onlyinsertion: flags.boolean({ char: 'i', description: messages.getMessage('onlyinsertionFlagDescription') }),
         onlychanged: flags.boolean({ char: 'c', description: messages.getMessage('onlychangedFlagDescription') })
@@ -54,81 +55,50 @@ export default class Changes extends SfdxCommand {
     protected static requiresProject = false;
 
     public async run(): Promise<AnyJson> {
+        const branch = this.flags.branch || 'remotes/origin/master';
+        const inputdir = this.flags.inputdir || 'src'; // TODO: change this to force-app
         const git: SimpleGit = simpleGit();
-        const setSSH = await git.env('GIT_SSH_COMMAND', GIT_SSH_COMMAND).status();
-        //console.log('setSSH:');
-        //console.log(setSSH);
-        const diffSum = await git.env({ ...process.env, GIT_SSH_COMMAND }).diffSummary(['remotes/origin/master']);
+        await git.env('GIT_SSH_COMMAND', GIT_SSH_COMMAND).status();
+        const diffSum = await git.env({ ...process.env, GIT_SSH_COMMAND }).diffSummary([branch]);
 
-        const diff = {
+        const result = {
             changed: [],
             insertion: [],
             destructive: []
         };
 
         diffSum.files.forEach(file => {
+            if (!file.file.startsWith(inputdir)) return;
             if (file.changes === file.insertions && file.deletions === 0 && !file.file.includes('=>')) {
-                diff.insertion = [...diff.insertion, file.file];
+                result.insertion = [...result.insertion, file.file];
             } else if (file.changes === file.deletions && !file.file.includes('=>')) {
-                diff.destructive = [...diff.destructive, file.file];
+                result.destructive = [...result.destructive, file.file];
             } else if (file.file.includes('=>')) {
                 const path = file.file.substring(0, file.file.indexOf('{'));
                 const files = file.file.substring(file.file.indexOf('{'));
                 const oldFile = path + files.substring(0, files.indexOf('=')).replace('{', '').trim();
                 const newFile = path + files.substring(files.indexOf('>') + 1).replace('}', '').trim();
-                diff.destructive = [...diff.destructive, oldFile];
-                diff.insertion = [...diff.insertion, newFile];
+                result.destructive = [...result.destructive, oldFile];
+                result.insertion = [...result.insertion, newFile];
             } else {
-                diff.changed = [...diff.changed, file.file];
+                result.changed = [...result.changed, file.file];
             }
         });
-        // const status: StatusResult = await git.status();
-
-        //   const branch = this.flags.branch || 'master';
-        //   const outputdir = this.flags.outputdir || 'parcel';
-
-        //   // this.org is guaranteed because requiresUsername=true, as opposed to supportsUsername
-        //   const conn = this.org.getConnection();
-        //   const query = 'Select Name, TrialExpirationDate from Organization';
-
-        //   // The type we are querying for
-        //   interface Organization {
-        //     Name: string;
-        //     TrialExpirationDate: string;
-        //   }
-
-        //   // Query the org
-        //   const result = await conn.query<Organization>(query);
-
-        //   // Organization will always return one result, but this is an example of throwing an error
-        //   // The output and --json will automatically be handled for you.
-        //   if (!result.records || result.records.length <= 0) {
-        //     throw new SfdxError(messages.getMessage('errorNoOrgResults', [this.org.getOrgId()]));
-        //   }
-
-        //   // Organization always only returns one result
-        //   const orgName = result.records[0].Name;
-        //   const trialExpirationDate = result.records[0].TrialExpirationDate;
-
-        // let outputString = `Hello`;
-        //   if (trialExpirationDate) {
-        //     const date = new Date(trialExpirationDate).toDateString();
-        //     outputString = `${outputString} and I will be around until ${date}!`;
-        //   }
-        //   this.ux.log(outputString);
-
-        //   // this.hubOrg is NOT guaranteed because supportsHubOrgUsername=true, as opposed to requiresHubOrgUsername.
-        //   if (this.hubOrg) {
-        //     const hubOrgId = this.hubOrg.getOrgId();
-        //     this.ux.log(`My hub org id is: ${hubOrgId}`);
-        //   }
-
-        //   if (this.flags.force && this.args.file) {
-        //     this.ux.log(`You input --force and a file: ${this.args.file}`);
-        //   }
-
-        //   // Return an object to be displayed with --json
-        console.log(diff);
-        return diff;
+        
+        const showAll = !this.flags.onlychanged && !this.flags.onlyinsertion && !this.flags.onlydestructive;
+        const whatToPrint = {
+            changed: this.flags.onlychanged,
+            insertion: this.flags.onlyinsertion,
+            destructive: this.flags.onlydestructive
+        };
+        
+        Object.keys(result).forEach(key => {
+            if(result[key].length === 0 && (whatToPrint[key] || showAll)){
+                this.ux.log(key + ': None Found')
+            } else if(whatToPrint[key] || showAll){
+                this.ux.log(key + ': ' + result[key]);
+            }
+        });
+        return result;
     }
 }
