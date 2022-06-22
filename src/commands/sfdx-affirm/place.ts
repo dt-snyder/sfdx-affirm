59346c7855baee513aa0692baeda501ef9fbf51d
Drew Snyder <drewtsnyder@gmail.com>
2022-06-22 16:39:41 -0600

    WIP/TODOs for place

--- src/commands/sfdx-affirm/place.ts
+++ src/commands/sfdx-affirm/place.ts
@@ -1,7 +1,10 @@
 import { flags, SfdxCommand } from '@salesforce/command';
 import { Messages, SfdxError } from '@salesforce/core';
 import * as fs from 'fs-extra';
-import { AnyJson } from '@salesforce/ts-types';
+import {
+  AnyJson,
+  // ensureAnyJson
+} from '@salesforce/ts-types';
 // import { gitDiffSum, getRemoteInfo, getCurrentBranchName } from '../../lib/affirm_git';
 import { getYNString, verifyUsername, getTestsFromPackageSettingsOrUser, liftCleanProvidedTests } from '../../lib/affirm_lift';
 // import { fsSaveJson, getPrintableDiffObject } from '../../lib/affirm_fs';
@@ -9,7 +12,9 @@ import { getYNString, verifyUsername, getTestsFromPackageSettingsOrUser, liftCle
 import { AffirmSettings } from '../../lib/affirm_interfaces';
 import { getAffirmSettings } from '../../lib/affirm_settings';
 import chalk = require('chalk');
-import { sfdxGetIsSandbox } from '../../lib/affirm_sfdx';
+import { sfdxGetIsSandbox, sfdxOpenToPath } from '../../lib/affirm_sfdx';
+// import { MetadataApiDeployStatus } from '@salesforce/source-deploy-retrieve';
+// import { runCommand } from '../../lib/sfdx';
 
 // Initialize Messages with the current plugin directory
 Messages.importMessagesDirectory(__dirname);
@@ -35,6 +40,9 @@ export default class Place extends SfdxCommand {
     waittime: flags.integer({ char: 'w', description: messages.getMessage('waittimeFlagDescription') }),
     noresults: flags.boolean({ char: 'r', description: messages.getMessage('noresultsFlagDescription') }),
     openstatus: flags.boolean({ char: 'o', description: messages.getMessage('openstatusFlagDescription') }),
+    saveresults: flags.boolean({ char: 'e', description: messages.getMessage('saveresultsFlagDescription') }),
+    printall: flags.boolean({ char: 'p', description: messages.getMessage('printallFlagDescription') }),
+    notestsrun: flags.boolean({ char: 'n', description: messages.getMessage('notestsrunFlagDescription') }),
   };
 
   // Comment this out if your command does not require an org username
@@ -64,16 +72,36 @@ export default class Place extends SfdxCommand {
       throw SfdxError.create('sfdx-affirm', 'place', errorType);
     }
     this.ux.log(`Package Directory: "${chalk.underline.blue(packagedir)}"`);
+    // TODO: add logic to get the default test class for production deployments
     const testclasses = this.flags.testclasses;
     let useTestClasses;
-    if (!testclasses) {
+    if (!testclasses && !this.flags.notestsrun) {
       useTestClasses = await getTestsFromPackageSettingsOrUser(this.ux, settings, packagedir, orgIsSandbox, silent);
-    } else {
+    } else if (!this.flags.notestsrun && testclasses) {
       useTestClasses = await liftCleanProvidedTests(testclasses);
+    }
+    // TODO: add flag and method that allows user to use a specific test suite
+    const testClassLog = useTestClasses ? 'Deployment Test Classes: ' : chalk.red('Deployment Will Not Run Tests!');
+    this.ux.log(testClassLog);
+    if (useTestClasses) {
+      const testClasses = useTestClasses.split(',');
+      for (const test of testClasses) {
+        this.ux.log(chalk.green(test));
+      }
     }
     // const waittime = this.flags.waittime || settings.waitTime;
     // const tests = useTestClasses ? ` -l RunSpecifiedTests -r ${useTestClasses}` : ' -l NoTestRun';
+    if (this.flags.openstatus) {
+      this.ux.log(`Opening Deployment Status page in ${chalk.greenBright(username)}`);
+      await sfdxOpenToPath(username, 'lightning/setup/DeployStatus/home', false);
+    }
+    this.ux.startSpinner('Validating Package');
+    // const command = `sfdx force:mdapi:deploy -u ${username} -d ${packagedir} ${tests} -w ${waittime}`;
+    // TODO: handle timeout more gracefully by implementing @salesforce/source-deploy-retrieve
+    // TODO: v3: add verbose flag that prints each of the sfdx commands that are run by this command.
+    // const validationResult: MetadataApiDeployStatus = ensureAnyJson(await runCommand(command))['result'] as unknown as MetadataApiDeployStatus;
     // TODO: finish this
     return { status: 'complete', classes: useTestClasses };
   }
 }
+
