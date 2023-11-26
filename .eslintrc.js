a070a40caa2509c7015dae58a185abb77123f359
Drew Snyder <drewtsnyder@gmail.com>
2023-11-26 12:38:14 -0700

    small updates to dev config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,4 +8,5 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   extends: ['eslint-config-salesforce-typescript', 'eslint-config-salesforce-license', 'plugin:sf-plugin/migration'],
+  root: true
 };
